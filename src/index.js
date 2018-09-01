945636c1facf55854c15d0ed0d4deb1185efda2f
jonbaxt <jonbaxt@gmail.com>
2018-09-01 03:22:25 -0600

    Changes

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Application from './Application';
 
+// Had a client side that didn't have a backend. spun up a backend very quickly using those steps and then configured into applicaiton through these imports.
+import Amplify from 'aws-amplify';
+import configuration from './aws-exports';
+
+Amplify.configure(configuration);
+
 ReactDOM.render(<Application />, document.getElementById('root'));
